e6855b680b7eed9a1f6e8c1eb4e05f9337da55af
James Valles <jamesvallesdotcom@gmail.com>
2019-11-05 14:01:19 -0600

    Added environments folder to gitignore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,4 +5,14 @@
 
 export const environment = {
   production: false,
+  firebase:{
+    apiKey: "AIzaSyAAemFDdqMMDH1dhkSj-0cxkVNIp9ZmWsQ",
+    authDomain: "chatapp-d58a0.firebaseapp.com",
+    databaseURL: "https://chatapp-d58a0.firebaseio.com",
+    projectId: "chatapp-d58a0",
+    storageBucket: "chatapp-d58a0.appspot.com",
+    messagingSenderId: "575695653601",
+    appId: "1:575695653601:web:92953ca539c695a8bcc5a0",
+    measurementId: "G-MNJ5ZF6ZT1"
+  }
 };
